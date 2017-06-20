ee16d43084a07f3e73df69b724a1ee58d8b32bb7
xinufo <1064752091@qq.com>
2017-06-20 17:44:15 +0800

    add insertion sort

---
+++ data_structure_and_algorithm/sort/InsertionSort.java
@@ -0,0 +1,37 @@
+package solution;
+
+/**
+ * 插入排序
+ * 
+ * 实现方式：迭代
+ * 时间复杂度 Ω(n)，O(n^2)
+ * 空间复杂度 O(1)
+ * 
+ * 原理： 
+ * 依次取出某个元素，将此元素之前的所有元素视为已经有序，将该元素插入到合适的位置使得插入后的序列仍然有序
+ */
+public class InsertionSort {
+
+	public static <T extends Comparable<T>> T[] sort(T[] arr) {
+		// 用于缓存要插入的元素
+		T tmp;
+		int j;
+
+		for (int i = 1; i < arr.length; ++i) {
+			tmp = arr[i];
+			// 遍历并移动之前元素
+			for (j = i - 1; j >= 0; --j) {
+				if (arr[j].compareTo(tmp) > 0) {
+					arr[j + 1] = arr[j];
+				} else {
+					break;
+				}
+			}
+			// 若之前移动过元素则插入
+			if (j >= 0)
+				arr[++j] = tmp;
+		}
+		return arr;
+	}
+
+}
